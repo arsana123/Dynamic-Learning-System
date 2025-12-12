1ace610ceced447896b135c894fbdd7b072e7ce0
archana <arsanasingh123@gmail.com>
2025-12-12 22:34:21 +0530

    refactor: remove unnecessary comment in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
         Your Learning Dashboard
       </motion.h1>
 
-      {/* Stats Cards */}
+
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
   
